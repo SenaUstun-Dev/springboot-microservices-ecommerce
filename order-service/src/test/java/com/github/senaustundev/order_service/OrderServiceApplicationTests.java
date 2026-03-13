783589a54a54e86b2f3cc1014239744b3c0979dd
SenaUstun-Dev <senaustun.dev@gmail.com>
2026-03-14 02:26:58 +0300

    feat: added place order tests and dependencies

--- order-service/src/test/java/com/github/senaustundev/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/github/senaustundev/order_service/OrderServiceApplicationTests.java
@@ -1,15 +1,62 @@
 package com.github.senaustundev.order_service;
+ 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
+import com.github.senaustundev.order_service.repository.OrderRepository;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+ 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
+ 
+	@LocalServerPort
+	private Integer port;
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private OrderRepository orderRepository;
+ 
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+		RestAssured.proxy = null;
 	}
+ 
+	@AfterEach
+	void tearDown() {
+		orderRepository.deleteAll();
+	}
+ 
+	@Test
+	void shouldPlaceOrder() {
+		String requestBody = """
+				{
+					"skuCode": "iphone_15",
+					"price": 45000,
+					"quantity": 1
+				}
+				""";
 
+		RestAssured.given()
+				.contentType(ContentType.JSON)
+				.body(requestBody)
+				.when()
+				.post("/api/orders")
+				.then()
+				.statusCode(201);
+
+		assertThat(orderRepository.findAll().size(), is(1));
+	}
+ 
 }
