a47d2a94523d49ebe7be53adbb6ed2677cc45851
SenaUstun-Dev <senaustun.dev@gmail.com>
2026-03-16 02:30:24 +0300

    feat: Created inventory repo

--- inventory-service/src/main/java/com/github/senaustundev/inventory_service/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/github/senaustundev/inventory_service/repository/InventoryRepository.java
@@ -1,5 +1,11 @@
 package com.github.senaustundev.inventory_service.repository;
 
-public interface InventoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.github.senaustundev.inventory_service.modul.Inventory;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    // Find an inventory for a given skuCode where quantity >= 0
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
 }
